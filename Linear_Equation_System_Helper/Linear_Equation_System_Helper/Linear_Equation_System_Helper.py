c6818d1c1b17ba6d778e08165bcfa3b6ae1d755f
kanos27 <54512112+kanos27@users.noreply.github.com>
2021-09-26 22:04:11 +0200

    added class equation and system
    added structure of equation and equation system (+main)

--- Linear_Equation_System_Helper/Linear_Equation_System_Helper/Linear_Equation_System_Helper.py
+++ Linear_Equation_System_Helper/Linear_Equation_System_Helper/Linear_Equation_System_Helper.py
@@ -1,1 +1,34 @@
 
+
+class equation():
+    def __init__(self, factors, second_member):
+        self.factors = factors
+        self.second_member = second_member
+
+    def __str__(self):
+        equation_text = ""
+        for i in range(len(self.factors)):
+            if i == 0 :
+                equation_text += str(self.factors[i]) + "x" + str(i+1)
+            else :
+                equation_text += " + " + str(self.factors[i]) + "x" + str(i+1)
+        equation_text += " = " + self.second_member
+
+        return equation_text
+
+
+class equation_system():
+    def __init__(self, equations, nbr_of_unknown):
+        self.equations = equations
+        self.nbr_of_unknown = nbr_of_unknown
+        ...
+
+    def __str__(self):
+        ...
+
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
