971a6c0ae20ee716fc9c5d318dd42da27d08b8e5
kanos27 <54512112+kanos27@users.noreply.github.com>
2021-09-27 10:18:52 +0200

    added basic equation operations
    added addition and multiplication of an equation, and added swapping between equation in a system, in order to apply the Gaussian elimination

--- Linear_Equation_System_Helper/Linear_Equation_System_Helper/Linear_Equation_System_Helper.py
+++ Linear_Equation_System_Helper/Linear_Equation_System_Helper/Linear_Equation_System_Helper.py
@@ -16,6 +16,14 @@ class equation():
 
         return equation_text
 
+    def __mul__(self, scaler):
+        for factor in range(len(self.factors)):
+            self.factors[factor]*= scaler
+
+    def __add__(self, other):
+        for factor in range(len(self.factors)):
+            self.factors[factor] += other.factors[factor]*scaler
+
 
 class equation_system():
     def __init__(self, equations, nbr_of_unknown):
@@ -25,6 +33,9 @@ class equation_system():
 
     def __str__(self):
         ...
+
+    def swap_equations(self, index1, index2):
+        self.equations[index1], self.equations[index2] = self.equations[index2], self.equations[index2]
 
 
 def main():
