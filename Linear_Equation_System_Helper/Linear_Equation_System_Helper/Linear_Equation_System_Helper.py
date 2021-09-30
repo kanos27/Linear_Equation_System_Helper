f99735fc08573364ef3cdd68a194bae2f0b4d3a6
kanos27 <54512112+kanos27@users.noreply.github.com>
2021-09-30 09:01:09 +0200

    added rmul and copy method
    Fixed issue of both variable changing with operation by copying initial variable in method. Added rmul method to allow both way multiplication. Issue with installing fractions package.

--- Linear_Equation_System_Helper/Linear_Equation_System_Helper/Linear_Equation_System_Helper.py
+++ Linear_Equation_System_Helper/Linear_Equation_System_Helper/Linear_Equation_System_Helper.py
@@ -1,4 +1,4 @@
-
+#Import fractions
 
 class Equation():
     def __init__(self, factors, second_member):
@@ -17,24 +17,35 @@ class Equation():
         return equation_text
 
     def __mul__(self, scaler):
-        new_equation = self
+        new_equation = self.copy()
+        for factor in range(len(self.factors)):
+            new_equation.factors[factor] = self.factors[factor] * scaler
+        new_equation.second_member = self.second_member * scaler
+        return new_equation
+
+    def __rmul__(self, scaler):
+        new_equation = self.copy()
         for factor in range(len(self.factors)):
             new_equation.factors[factor] = self.factors[factor] * scaler
         new_equation.second_member = self.second_member * scaler
         return new_equation
 
     def __add__(self, other):
-        new_equation = self
+        new_equation = self.copy()
         for factor in range(len(self.factors)):
             new_equation.factors[factor] = self.factors[factor] + other.factors[factor]
         new_equation.second_member = self.second_member + other.second_member
         return new_equation
 
     def __sub__(self,other):
-        new_equation = self
+        new_equation = self.copy()
         for factor in range(len(self.factors)):
             new_equation.factors[factor] = self.factors[factor] - other.factors[factor]
         new_equation.second_member = self.second_member - other.second_member
+        return new_equation
+
+    def copy(self):
+        new_equation = Equation([n for n in self.factors], self.second_member)
         return new_equation
 
 class Equation_system():
@@ -67,7 +78,7 @@ def main():
     print(sys1)
     sys1.equations[0] = sys1.equations[0] - sys1.equations[1]
     print(sys1)
-    sys1.equations[0] = sys1.equations[0] - (sys1.equations[1]*2)
+    sys1.equations[0] = sys1.equations[0] - (2*sys1.equations[1])
     print(sys1)
 
 if __name__ == "__main__":
